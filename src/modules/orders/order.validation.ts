9b6ae541e6c468226478996b4c0fce3474e39464
Rabby <mdrabbybd4747@gmail.com>
2024-05-22 18:24:30 +0600

    added validation message

--- src/modules/orders/order.validation.ts
+++ src/modules/orders/order.validation.ts
@@ -2,10 +2,24 @@ import { z } from "zod"
 
 // order validation
 const orderValidationSchema = z.object({
-	email: z.string(),
-	productId: z.string(),
-	price: z.number(),
-	quantity: z.number(),
+	email: z.string({
+		required_error: "email is required",
+		invalid_type_error: "email Must be a String",
+	}),
+	productId: z
+		.string({
+			required_error: "productId is required",
+			invalid_type_error: "productId Must be a String",
+		})
+		.email(),
+	price: z.number({
+		required_error: "price is required",
+		invalid_type_error: "price Must be a Number",
+	}),
+	quantity: z.number({
+		required_error: "quantity is required",
+		invalid_type_error: "quantity Must be a Number",
+	}),
 })
 
 export default orderValidationSchema
--- src/modules/products/product.validation.ts
+++ src/modules/products/product.validation.ts
@@ -2,22 +2,46 @@ import { z } from "zod"
 
 // variant validation schema with zod
 const variationValidationSchema = z.object({
-	type: z.string(),
-	value: z.string(),
+	type: z.string({
+		required_error: "type is required",
+		invalid_type_error: "type Must be a String",
+	}),
+	value: z.string({
+		required_error: "value is required",
+		invalid_type_error: "value Must be a String",
+	}),
 })
 
 // inventory validation schema with zod
 const inventoryValidationSchema = z.object({
-	quantity: z.number(),
-	inStock: z.boolean(),
+	quantity: z.number({
+		required_error: "quantity is required",
+		invalid_type_error: "quantity Must be a Number",
+	}),
+	inStock: z.boolean({
+		required_error: "inStock is required",
+		invalid_type_error: "inStock Must be a Boolean",
+	}),
 })
 
 // product validation schema with zod
 const productValidationSchema = z.object({
-	name: z.string(),
-	description: z.string(),
-	price: z.number(),
-	category: z.string(),
+	name: z.string({
+		required_error: "name is required",
+		invalid_type_error: "name Must be a String",
+	}),
+	description: z.string({
+		required_error: "description is required",
+		invalid_type_error: "description Must be a String",
+	}),
+	price: z.number({
+		required_error: "price is required",
+		invalid_type_error: "price Must be a Number",
+	}),
+	category: z.string({
+		required_error: "category is required",
+		invalid_type_error: "category Must be a String",
+	}),
 	tags: z.array(z.string()),
 	variants: z.array(variationValidationSchema),
 	inventory: inventoryValidationSchema,
