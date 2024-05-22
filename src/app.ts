318d5cdebdff4770282d9a00dfc6174272296d7e
Rabby <mdrabbybd4747@gmail.com>
2024-05-22 12:27:28 +0600

    add route not found error for global and remove unnecesarry package

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express, { Application, Request, Response } from "express"
-import cors from "cors"
 import { ProductRoute } from "./modules/products/product.route"
 import { OrderRoute } from "./modules/orders/order.route"
 const app: Application = express()
@@ -7,13 +6,18 @@ const app: Application = express()
 // parser
 app.use(express.json())
 
-app.use(cors())
-
 app.use("/api/products", ProductRoute)
 app.use("/api/orders", OrderRoute)
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Hello World!")
+})
+
+app.all("*", (req: Request, res: Response) => {
+	res.json({
+		success: false,
+		message: "Route not found",
+	})
 })
 
 export default app
